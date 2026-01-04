01ecb77205e18902aea7ee783f746293bf5efd09
maniemagz-dotcom <maniemagz@gmail.com>
2026-01-04 23:28:50 +0300

    Creation of Experiment_1 : Loops
    Created the first experiment using p5.js. A combination of tasks 1 and 2.

---
+++ Experiment_1-loops/sketch.js
@@ -0,0 +1,41 @@
+// Experiment 1: Combination of task 1 and 2
+
+// Task 1
+let pos;
+
+function setup() {
+  createCanvas(400,500); // x,y
+  // Extension for task 1
+  // using random(range) to change the position variable every time the sketch runs
+  pos = random(200,300);
+}
+
+function draw() {
+  background(135,206,250); 
+  // Task 2
+  // Extension for task 2
+  let spacing = 80;
+  
+  for (let x = 0; x<= width; x += spacing){
+    for (let y = 0; y <= height; y += spacing){
+      
+  // ellipse : moves vertically
+  fill(204, 255, 255);
+  ellipse(x, y, 70, 60); // x,y,w,h
+    }
+  }
+
+// Continuation of task 1
+  // circle : moves horizontally
+  fill(0,204,255); // colour
+  circle(pos, 200, 90); // x,y,d
+  
+  // rectangle : moves vertically
+  fill(255, 255, 255); // colour
+  rect(150, pos, 50, 55); // x,y,l,w
+  
+  // triangle : stretches a corner horizontally
+  fill(0,51,102); // colour
+  triangle(150, 150, 200, 120, pos, 200); 
+  //x1, y1, x2, y2, x3, y3
+}
